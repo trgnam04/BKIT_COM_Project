48825929ca9920ef97cdd5f560fb7da2434c2675
unknown <trungnamtbcm123@gmail.com>
2025-02-08 11:01:00 +0700

    add BKIT COM State Typedef

--- Core/Inc/HW_Layer.h
+++ Core/Inc/HW_Layer.h
@@ -8,6 +8,13 @@
 #ifndef INC_HW_LAYER_H_
 #define INC_HW_LAYER_H_
 
+extern enum{
+	BKIT_COM_OK 		= 	0x00U,
+	BKIT_COM_ERROR		=	0x01U,	// Transmit in HW layer fail
+	BKIT_COM_BUSY		=	0x02U,	// Waiting for message
+	BKIT_COM_TIMEOUT	=	0x03U,
+	BKIT_COM_FAIL		=	0x04U	// Wrong Message or Missing Message
+} BKIT_COM_StateTypedef;
 
 
 #endif /* INC_HW_LAYER_H_ */
