7c1505a9376361c9b661f5058c87fc24d3bfd1a4
Tran Xuan Nam <tranxuannam02@gmail.com>
2025-02-08 12:06:09 +0700

    Serialize data using nanopb in BKIT_SendMessage()

--- Core/Inc/Application_Layer.h
+++ Core/Inc/Application_Layer.h
@@ -9,6 +9,10 @@
 #define INC_APPLICATION_LAYER_H_
 
 #include "data_type.pb.h"
+#include "pb.h"
+#include "pb_encode.h"
+#include "pb_decode.h"
+#include "pb_common.h"
 
 uint16_t BKIT_Init();
 uint16_t BKIT_SendMessage(const sensor_data_t* data);
--- Core/Src/Application_Layer.c
+++ Core/Src/Application_Layer.c
@@ -14,6 +14,22 @@ uint16_t BKIT_Init()
 
 uint16_t BKIT_SendMessage(const sensor_data_t* data)
 {
+	// Duplicate sensor id
+	uint32_t sensor_id = data->sensor_id;
+
+	// Serialize
+	uint8_t buffer[sensor_data_t_size]; // Use the defined max size
+	pb_ostream_t ostream = pb_ostream_from_buffer(buffer, sizeof(buffer));
+
+	bool status = pb_encode(&ostream, &sensor_data_t_msg, data);
+
+	if (!status) {
+		// ERROR CODE: 0xFFFE
+		return 0xFFFE;
+	}
+	size_t message_length = ostream.bytes_written;
+
+	// Call message processing layer functions
 	return 0;
 }
 
